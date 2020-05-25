11ec9380d880149e8753719923bb7de4922843f7
zhangbwe <zhangbwe@yonyou.com>
2020-05-25 22:46:47 +0800

    flutter引入charts_flutter: ^0.9.0实现柱状图

--- lib/pages/tabs/Home.dart
+++ lib/pages/tabs/Home.dart
@@ -1,13 +1,103 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatefulWidget{
-  HomePage({Key key}) : super(key:key);
+//引入插件
+import 'package:charts_flutter/flutter.dart' as charts;
+
+class HomePage extends StatefulWidget {
+  HomePage({Key key}) : super(key: key);
+
   _HomePageState createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage>{
+class _HomePageState extends State<HomePage> {
+  String _year;
+  int _sales;
+
+  //点击柱状图触发的函数
+  _onSelectionChanged(charts.SelectionModel model) {
+    final selectedDatum = model.selectedDatum;
+//    print(selectedDatum.first.datum.year);
+//    print(selectedDatum.first.datum.sales);
+//    print(selectedDatum.first.series.displayName);
+    setState(() {
+      //改变两个显示的数值
+      _year = selectedDatum.first.datum.year;
+      _sales = selectedDatum.first.datum.sales;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text("首页");
+    return new Scaffold(
+        appBar: AppBar(
+          title: Text("图表"),
+          centerTitle: true,
+        ),
+        body: Container(
+          child: Column(
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Expanded(
+                    child: Container(
+                      alignment: Alignment.center,
+                      child: Text("年份：${_year}"),
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      alignment: Alignment.center,
+                      child: Text("数值：${_sales}"),
+                    ),
+                  )
+                ],
+              ),
+              Container(
+                width: double.infinity,
+                height: 200.0,
+                child: charts.BarChart(
+                  //通过下面获取数据传入
+                  ChartFlutterBean.createSampleData(),
+                  //配置项，以及设置触发的函数
+                  selectionModels: [
+                    charts.SelectionModelConfig(
+                      type: charts.SelectionModelType.info,
+                      changedListener: _onSelectionChanged,
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ));
+  }
+}
+
+//一下为组合柱状图数据部分
+class OrdinalSales {
+  final String year;
+  final int sales;
+
+  OrdinalSales(this.year, this.sales);
+}
+
+class ChartFlutterBean {
+  static List<charts.Series<OrdinalSales, String>> createSampleData() {
+    final data = [
+      new OrdinalSales('2014', 5),
+      new OrdinalSales('2015', 25),
+      new OrdinalSales('2016', 100),
+      new OrdinalSales('2017', 75),
+    ];
+
+    return [
+      new charts.Series<OrdinalSales, String>(
+        id: 'Sales',
+        colorFn: (_, __) => charts.MaterialPalette.blue.shadeDefault,
+        domainFn: (OrdinalSales sales, _) => sales.year,
+        measureFn: (OrdinalSales sales, _) => sales.sales,
+        data: data,
+      )
+    ];
   }
 }
